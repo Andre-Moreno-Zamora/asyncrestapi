9ce10c4c8cae4e20ae3a112261365c8d1dcfc6a7
Andre-Moreno-Zamora <andre.moreno@atos.net>
2022-03-11 11:55:47 -0600

    Added spring boot project files

---
+++ src/main/java/com/example/asyncrestapi/AsyncrestapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.asyncrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncrestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncrestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/asyncrestapi/AsyncrestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.asyncrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncrestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
